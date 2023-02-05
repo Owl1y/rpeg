c26d8c30dd77c4f76c7f326a2267244df6f8f6e2
aj <ali.becerra1224@gmail.com>
2023-02-04 19:57:17 -0800

    added photo to work on, the hex of the photo in txt, and starting the proj with the hashmap of the hexadecimal values

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use std::collections::HashMap;
+
 fn main() {
-       
-    let x: () = 0xfff;
+    
+    let marker_mapping = HashMap::from([
+        (0xffd8, "Start of Image"),
+        (0xffe0, "Application of default header"),
+        (0xffdb, "Quantization table"),
+        (0xffc0, "Start of frame"),
+        (0xffc4, "Define huffman table"),
+        (0xffda, "Start of scan"),
+        (0xffd9, "end of image"),
+    ]);
+    
+    println!("{:?}", marker_mapping); 
+
 
 }
 
